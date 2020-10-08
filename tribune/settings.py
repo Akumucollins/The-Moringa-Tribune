31ac08a707bd0bcf3d2272d213931a5c95b3249f
Akumucollins <akumucollins@gmail.com>
2020-10-08 10:14:19 +0300

    corrected the setting.py file

--- tribune/settings.py
+++ tribune/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-MODE=config("MODE", default="dev")
+MODE=config("MODE", default="prod")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', default=True)
