9b2afd7ee908c81ee9e13f946380dce6fcc85aae
Akumucollins <akumucollins@gmail.com>
2020-10-08 10:06:19 +0300

    corrected the setting.py file

--- tribune/settings.py
+++ tribune/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MODE=config("MODE", default="dev")
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default=True)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
